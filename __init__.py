b754313e289294caa25d9895c9ac6e7d7660d681
alBz <me@albz.dev>
2023-11-24 22:55:35 +0100

    Added Blog

--- __init__.py
+++ __init__.py
@@ -17,6 +17,10 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
